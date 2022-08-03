d5ee216bab3275e2ccf91caed8f18f3ecc29a9fb
HoonyHoney-91 <shinx333@morris.umn.edu>
2022-08-03 10:03:24 -0700

    adding weather and fnshin off todo

--- JS/background.js
+++ JS/background.js
@@ -3,10 +3,7 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage=document.createElement("img");
 
-<<<<<<< HEAD
-=======
 console.log(bgImage);
 
->>>>>>> 60093e9bd7422495bd689ab24bc25fddafb9bdba
 bgImage.src= `img/${chosenImage}`;
 document.body.appendChild(bgImage);
--- JS/todo.js
+++ JS/todo.js
@@ -12,10 +12,8 @@ function saveToDos() {
 
 function deleteToDo(event) {
     const li = event.target.parentElement;
-<<<<<<< HEAD
-    // console.log(li.id);
-=======
->>>>>>> 60093e9bd7422495bd689ab24bc25fddafb9bdba
+    toDos = toDos.filter(toDo => toDo.id !==parseInt(li.id));
+    saveToDos()
     li.remove();
 }
 
@@ -23,11 +21,7 @@ function paintToDo(newTodo){
     const li = document.createElement("li");
     li.id = newTodo.id;
     const span = document.createElement("span");
-<<<<<<< HEAD
     span.innerText = newTodo.text;
-=======
-    span.innerText = newTodo;
->>>>>>> 60093e9bd7422495bd689ab24bc25fddafb9bdba
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteToDo);
@@ -37,7 +31,6 @@ function paintToDo(newTodo){
 }
 
 function handleToDoSubmit(event) {
-<<<<<<< HEAD
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = "";
@@ -57,22 +50,5 @@ if (savedToDos !== null) {
   const parsedToDos = JSON.parse(savedToDos);
   toDos = parsedToDos;
   parsedToDos.forEach(paintToDo);
-=======
-    event.preventDefault();  
-    const newToDo = toDoInput.value;
-    toDoInput.value = "";
-    toDos.push(newToDo);
-    paintToDo(newToDo);
-    saveTodos();
-};
-
-toDoForm.addEventListener("submit", handleToDoSubmit);
-
-const savedToDos = localStorage.getItem(TODOS_KEY);
-
-if (savedToDos !== null) {
-    const parsedToDos = JSON.parse(savedToDos);
-    toDos = parsedToDos;
-    parsedToDos.forEach(paintToDo);
->>>>>>> 60093e9bd7422495bd689ab24bc25fddafb9bdba
 }
+
